0613c07e12ce96bb96f49af2d8bd9a90e98ba118
Filipe Ribeiro <filipepaiva240@gmail.com>
2022-02-11 10:55:21 -0300

    Fix layout shifting on poke images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  reactStrictMode: true
-  // i18n: {
-  //   locales: ["pt-BR", "en-US"],
-  //   defaultLocale: "en-US"
-  // }
+  reactStrictMode: true,
+  images: {
+    domains: ["images.pokemontcg.io"]
+  }
 };
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,19 @@
 import styles from "./styles.module.scss";
 import Link from "next/link";
+import Image from "next/image";
 
 export function Card({ images, name, id, type }) {
   return (
-    <Link href={`/${id}`}>
+    <Link href={`/${id}`} passHref>
       <article className={styles.container}>
-        <img className={styles.image} src={images.small} alt={name} />
+        <div className={styles.image}>
+          <Image
+            layout="fill"
+            objectFit="contain"
+            src={images.small}
+            alt={name}
+          />
+        </div>
         <p className={styles.name}>Name: {name}</p>
         <p className={styles.type}>Type: {type}</p>
         <p className={styles.id}>Id: {id}</p>
